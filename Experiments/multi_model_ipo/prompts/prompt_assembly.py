066857bfdcad870415d569033261bbe0abbbf181
NAthan <nathanbsmith729@gmail.com>
2026-05-20 12:22:48 -0500

    Create `prompt_assembly.py`

---
+++ Experiments/multi_model_ipo/prompts/prompt_assembly.py
@@ -0,0 +1,47 @@
+from ..prompt_orchestration.get_prompt_data import *
+from ..prompts.deep_research_prompt import *
+from ..prompts.daily_prompt import *
+
+
+
+def create_deep_research_prompt(libb: LIBBmodel):
+
+    prompt_skeleton: str = assemble_deep_research_prompt_skeleton()
+
+    today = libb.run_date
+
+    portfolio_state = libb.portfolio
+    portfolio_tickers_eligibility = build_eligibility_series(portfolio_state["ticker"])
+    execution_log = libb.recent_execution_logs()
+
+    if execution_log.empty:
+        execution_log = "No recent trade logs."
+
+    prompt = prompt_skeleton.format(
+        today=today,
+        PORTFOLIO_STATE=portfolio_state,
+        PORTFOLIO_TICKER_ELIGIBILITY=portfolio_tickers_eligibility,
+        TRADE_EXECUTION_LOG=execution_log
+        )
+
+    return prompt
+
+def create_daily_prompt(libb: LIBBmodel): 
+
+    prompt_skeleton = assemble_daily_prompt_skeleton()
+
+    today = libb.run_date
+    portfolio_state = libb.portfolio
+    portfolio_eligibility = build_eligibility_series(portfolio_state["ticker"])
+    execution_log = libb.recent_execution_logs()
+
+    if execution_log.empty:
+        execution_log = "No recent trade logs."
+
+    prompt = prompt_skeleton.format(
+            today=today,
+            PORTFOLIO_STATE=portfolio_state,
+            TRADE_EXECUTION_LOG=execution_log,
+            PORTFOLIO_TICKER_ELIGIBILITY=portfolio_eligibility)
+
+    return prompt
